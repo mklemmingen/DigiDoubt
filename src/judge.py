b2e8582aa2a3ebaa917120addea3abbabe05ed9c
Marty Lauterbach <LauterbachMK@protonmail.com>
2023-07-31 16:03:31 +0200

    created project structure

---
+++ src/judge.py
@@ -0,0 +1,5 @@
+"""
+This document contains the judging algorithms for the scraped articles.
+Created by Marty Lauterbach on the 31st of July 2023, Warsaw, Poland.
+"""
+
--- src/search_engine.py
+++ src/search_engine.py
@@ -1,1 +1,9 @@
+"""
+This document contains the scraper for the search engine as well as a scraper of the currently biggest trends, that are then put into the search engine.
 
+Created by Marty Lauterbach on the 31st of July 2023, Warsaw, Poland.
+"""
+
+from pytrends.request import TrendReq
+
+pytrends = TrendReq(hl='en-US', tz=360)
